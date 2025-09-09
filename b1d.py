0da364d33f1cca44b08523dd99eac1a1776164a9
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2025-09-09 16:33:25 -0400

    Create b1d.py

---
+++ b1d.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def B1D(xi, nn):
+    """
+    B1D: Derivatives of 1D shape functions
+    xi : location in parent domain [-1, 1]
+    nn : number of nodes (2 or 3)
+    
+    Returns:
+    B : list of shape function derivatives
+    """
+    if nn == 2:
+        B = [-0.5, 0.5]
+    elif nn == 3:
+        B = [xi - 0.5, -2 * xi, xi + 0.5]
+    else:
+        raise ValueError('Only 2-node or 3-node elements supported')
+    
+    return B
