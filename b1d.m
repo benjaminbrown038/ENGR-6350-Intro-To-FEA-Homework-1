35f64ad602d411ee7709ae3dac2faf4deaa1bcf0
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2025-09-17 20:09:38 -0400

    Update b1d.m

--- b1d.m
+++ b1d.m
@@ -1,9 +1,9 @@
-function B = B1D(xi, nn)
+function B = B1d(nn,xi)
     % B1D: Derivatives of 1D shape functions
     % xi: location in parent domain [-1,1]
     % nn: number of nodes (2 or 3)
     
-    if nn == 2
+    if nn == 2;
         B = [-0.5, 0.5];
     elseif nn == 3
         B = [xi-0.5, -2*xi, xi+0.5];
